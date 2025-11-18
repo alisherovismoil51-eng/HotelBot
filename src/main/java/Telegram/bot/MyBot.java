eace337ea94658c6e048b3522c39ce5ae5324fdf
baxromm <shamsutdinovbahrom4@gmail.com>
2025-11-18 10:34:56 +0500

    add project

--- src/main/java/Telegram/bot/MyBot.java
+++ src/main/java/Telegram/bot/MyBot.java
@@ -6,8 +6,21 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 public class MyBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
 
+            Long chatId = update.getMessage().getChatId();
+            String text = update.getMessage().getText();
+
+            String firstName = update.getMessage().getChat().getFirstName();
+            String lastName = update.getMessage().getChat().getLastName();
+
+            System.out.println("Firstname  :  " + firstName +
+                    "\nLastname : " + lastName +
+                    "\nChatId : " + chatId +
+                    "\nText : " + text);
+        }
     }
+
 
     @Override
     public String getBotUsername() {
